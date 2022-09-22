f22ded122932be71c128749489d526809911aec7
Phat Huynh <kimphat.devcs@gmail.com>
2022-09-23 00:03:10 +0700

    Update Header => Card

--- components/Main/Card/Card.js
+++ components/Main/Card/Card.js
@@ -1,30 +1,30 @@
 import Image from "next/image";
-import Phone3 from "../../../static/phone3.png";
 
 const styles = {
-  wrapper: "mx-auto flex max-h-[50rem] justify-center gap-[6rem]",
-  imageContainer: "",
-  image:"",
-  mainContent: "flex flex-col justify-center",
-  subTitle: "",
-  title: "",
-  detail: "",
+  wrapper:
+    "mx-auto flex flex-col-reverse sm:flex-row max-h-[50rem] justify-center gap-[1rem]",
+  wrapperReverse:
+    "mx-auto flex flex-col-reverse sm:flex-row-reverse max-h-[50rem] justify-center gap-[1rem]",
+  imageContainer: "ml-[4rem]",
+  image: "object-cover",
+  mainContent: "flex flex-col justify-center items-center sm:items-start ",
+  subTitle: "font-semibold text-[#FA4A0C] text-xl sm:py-2 py-4 px-2",
+  title: "font-bold text-3xl sm:max-w-[31rem] text-center sm:text-left px-2",
+  detail: "sm:py-2 py-4 text-[#737373] text-center sm:text-left px-4 sm:px-2",
 };
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={props.item.isRevert ? styles.wrapperReverse : styles.wrapper}
+    >
       <div className={styles.imageContainer}>
-        <Image className={styles.image} src={Phone3} alt={""} />
+        <Image className={styles.image} src={props.item.photo} alt={""} />
       </div>
       <div className={styles.mainContent}>
-        <div className={styles.subTitle}>Create an account</div>
-        <div className={styles.title}>
-          Create/login to an existing account to get started
-        </div>
-        <div className={styles.detail}>
-          An account is created with your email and a desired password
-        </div>
+        <div className={styles.subTitle}>{props.item.subTitle}</div>
+        <div className={styles.title}>{props.item.title}</div>
+        <div className={styles.detail}>{props.item.detail}</div>
       </div>
     </div>
   );
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,14 +1,46 @@
 import Card from "./Card/Card";
+import Phone3 from "../../static/phone3.png";
+import Phone4 from "../../static/phone4.png";
+import Phone5 from "../../static/phone5.png";
+
 const styles = {
   wrapper: "mx-auto",
   title: "font-bold text-center text-3xl py-8",
 };
 
 export default function Main() {
+  const dataSets = [
+    {
+      id: 1,
+      subTitle: "Create an account",
+      title: "Create/login to an existing account to get started",
+      detail: "An account is created with your email and a desired password",
+      photo: Phone3,
+      isRevert: false,
+    },
+    {
+      id: 2,
+      subTitle: "Explore varieties",
+      title: "Shop for your favorites meal as e dey hot.",
+      detail:
+        "Shop for your favorite meals or drinks and enjoy while doing it.",
+      photo: Phone4,
+      isRevert: true,
+    },
+    {
+      id: 3,
+      subTitle: "Checkout",
+      title: "When you done check out and get it delivered.",
+      detail: "When you done check out and get it delivered with ease.",
+      photo: Phone5,
+      isRevert: false,
+    },
+  ];
+  const listItems = dataSets.map((item) => <Card key={item.id} item={item} />);
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>How the app works</div>
-      <Card />
+      {listItems}
     </div>
   );
 }
